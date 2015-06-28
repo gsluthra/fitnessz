fd26f16bae2448096af49184867c599fc33543c7
Gurpreet Luthra <gluthra@thoughtworks.com>
2015-06-28 19:32:31 +0530

    Introduced the module pattern. Now clicking on YES prints the current selected date in console

--- src/main/webapp/js/track-panel.js
+++ src/main/webapp/js/track-panel.js
@@ -1,6 +1,42 @@
+var dayTrackEvent = (function () {
+    //PRIVATE VARIABLES AND FUNCTIONS
+    var currentSelectedDate;
+
+    logToConsole = function( foo ) {
+        console.log( foo );
+    };
+
+    return {
+        // PUBLIC FUNCTIONS
+        storeCurrentDate: function( momentDate ) {
+            currentSelectedDate = momentDate;
+        },
+
+        getDateForDisplay: function() {
+            //Refer to http://momentjs.com/
+            var day = currentSelectedDate.format('dddd');
+            var dateString = currentSelectedDate.format('LL') + " (" + day + ")";
+            return dateString;
+        },
+
+        getDateOnly: function() {
+            var dateOnlyString = currentSelectedDate.format('LL');
+            return dateOnlyString;
+        }
+    };
+
+})();
+
 function setDateSelected(momentDate) {
-    //Refer to http://momentjs.com/
-    var day = momentDate.format('dddd');
-    var dateString = momentDate.format('LL') + " (" + day + ")";
-    $("#dateSelected").text(dateString);
+    dayTrackEvent.storeCurrentDate(momentDate);
+    $("#dateSelected").text(dayTrackEvent.getDateForDisplay());
 }
+
+$(document).ready(function() {
+    $("#yesToExerciseBtn").on("click",function () {
+        console.log("Yes clicked for date: " + dayTrackEvent.getDateOnly());
+        var $yesButton = $("#yesToExerciseBtn");
+        $yesButton.removeClass("btn-default");
+        $yesButton.addClass("btn-success");
+     });
+ });
